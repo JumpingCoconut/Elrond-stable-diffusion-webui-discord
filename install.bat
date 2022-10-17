e02ac228405a57778394a9041c6a1d49f89f98a7
JCo <huepfende_kokosnuss@gmx.de>
2022-10-18 00:01:13 +0200

    Gitignore changed
    Also made some install cooler

--- install.bat
+++ install.bat
@@ -1,4 +1,11 @@
+@echo off
+
+rem Install the python virtual environment. 
 python -m venv venv
+rem Note: If multiple python versions are installed, use this command instead to use python 3.10
+rem c:/python310/python.exe -m venv venv
+
+rem Activate environment and install requirements
 call venv/Scripts/activate.bat
 python.exe -m pip install --upgrade pip
 python.exe -m pip install -r requirements.txt
--- run-elrond.bat
+++ run-elrond.bat
@@ -1,4 +1,5 @@
 @echo off
-echo Dont forget to enter your discord-bot token in the file .env!
-start /B cmd /C ".\venv\Scripts\activate.bat && python elrond.py" 
+echo Dont forget to enter your discord-bot token in the file .env! 
+echo Starting bot...
+start /B cmd /C ".\venv\Scripts\activate.bat && python elrond.py"
 pause
